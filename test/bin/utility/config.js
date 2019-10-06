44a7c0163a2b20718cb8da23d2be17c6b0e4cec4
Cyokin Zhang <half_6@hotmail.com>
2019-10-05 23:50:34 -0700

    fix ci issue

--- test/bin/utility/config.js
+++ test/bin/utility/config.js
@@ -14,7 +14,7 @@ describe('Unit Test -- utility/config.js',function () {
 	        $expect(config_dev.config.env).equal(`test-${process.env.NODE_ENV}-a`);
 	        $expect(config_dev.config.lfTemp).equal(`test-${process.env.LF_TEMP}-b`);
 	        $expect(config_dev.config.lfTest).equal(`test-${process.env.UNIT_TEST}-d`);
-            $expect(config_dev.$env).to.equal(process.env.NODE_ENV);
+            $expect(config_dev.$env).to.equal("dev");
 
             let config_prod = $lf.$config.readEnvConfig('prod', $meta);
             $lf.$logger.silly(config_prod);
